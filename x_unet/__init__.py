7dc5a9d7390384d2324127e03a091248472f0f7c
Phil Wang <lucidrains@gmail.com>
2022-07-24 09:04:10 -0700

    for now keep RSU separate as it is still only 2D compatible

--- x_unet/__init__.py
+++ x_unet/__init__.py
@@ -1,1 +1,1 @@
-from x_unet.x_unet import XUnet, NestedResidualUnet
+from x_unet.x_unet import XUnet, NestedResidualUnet2D
--- x_unet/x_unet.py
+++ x_unet/x_unet.py
@@ -22,32 +22,6 @@ def is_power_two(n):
 def Upsample(dim):
     return nn.ConvTranspose3d(dim, dim, (1, 4, 4), (1, 2, 2), (0, 1, 1))
 
-class PixelShuffleUpsample(nn.Module):
-    def __init__(self, dim, dim_out = None):
-        super().__init__()
-        dim_out = default(dim_out, dim)
-        conv = nn.Conv2d(dim, dim_out * 4, 1)
-
-        self.net = nn.Sequential(
-            conv,
-            nn.SiLU(),
-            nn.PixelShuffle(2)
-        )
-
-        self.init_conv_(conv)
-
-    def init_conv_(self, conv):
-        o, i, h, w = conv.weight.shape
-        conv_weight = torch.empty(o // 4, i, h, w)
-        nn.init.kaiming_uniform_(conv_weight)
-        conv_weight = repeat(conv_weight, 'o ... -> (o 4) ...')
-
-        conv.weight.data.copy_(conv_weight)
-        nn.init.zeros_(conv.bias.data)
-
-    def forward(self, x):
-        return self.net(x)
-
 def Downsample(dim):
     return nn.Conv3d(dim, dim, (1, 4, 4), (1, 2, 2), (0, 1, 1))
 
@@ -214,7 +188,33 @@ class XUnet(nn.Module):
 
 # RSU
 
-class NestedResidualUnet(nn.Module):
+class PixelShuffleUpsample2D(nn.Module):
+    def __init__(self, dim, dim_out = None):
+        super().__init__()
+        dim_out = default(dim_out, dim)
+        conv = nn.Conv2d(dim, dim_out * 4, 1)
+
+        self.net = nn.Sequential(
+            conv,
+            nn.SiLU(),
+            nn.PixelShuffle(2)
+        )
+
+        self.init_conv_(conv)
+
+    def init_conv_(self, conv):
+        o, i, h, w = conv.weight.shape
+        conv_weight = torch.empty(o // 4, i, h, w)
+        nn.init.kaiming_uniform_(conv_weight)
+        conv_weight = repeat(conv_weight, 'o ... -> (o 4) ...')
+
+        conv.weight.data.copy_(conv_weight)
+        nn.init.zeros_(conv.bias.data)
+
+    def forward(self, x):
+        return self.net(x)
+
+class NestedResidualUnet2D(nn.Module):
     def __init__(
         self,
         dim,
@@ -241,7 +241,7 @@ class NestedResidualUnet(nn.Module):
             )
 
             up = nn.Sequential(
-                PixelShuffleUpsample(2 * M, dim_in),
+                PixelShuffleUpsample2D(2 * M, dim_in),
                 nn.GroupNorm(groups, dim_in),
                 nn.SiLU()
             )
